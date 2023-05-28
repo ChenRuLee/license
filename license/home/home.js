342f288c6bcabd54f5990ea3ae7c5b6b418ac5ce
ChenRuLee <123973509+ChenRuLee@users.noreply.github.com>
2023-05-28 18:11:51 +0800

    Update home.js

--- license/home/home.js
+++ license/home/home.js
@@ -69,6 +69,11 @@ if (n < 1) { slideIndex = slides.length }
 for (i = 0; i < slides.length; i++) {
   slides[i].style.display = "none";
 }
+  // slides.forEach(function(slides, i) {
+//   console.log(i, slides)
+//     slides[i].style.display = "none";
+// });
+  
 for (i = 0; i < dots.length; i++) {
   dots[i].className = dots[i].className.replace(" active", "");
 }
